d2690f65e72832faf55ab57faaaa3135c1b1b035
Jose Miguel Flores Iduarte <josefloresiduarte97@gmail.com>
2024-10-03 14:20:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const ws = new WebSocket('wss://irc-ws.chat.twitch.tv:443');
 ws.onopen = function () {
     // Autenticación básica para Twitch
     ws.send('PASS oauth:mtt827eirkwdlfshpvvmb8923g3ofq'); // Reemplaza con tu token OAuth
-    ws.send('eren0411os'); // Reemplaza con tu nombre de usuario
+    ws.send('Eren0411os'); // Reemplaza con tu nombre de usuario
     ws.send('JOIN eren0411os'); // Reemplaza con tu canal (sin la #)
 };
 
